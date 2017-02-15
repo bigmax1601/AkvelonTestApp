e5c7a88f15e7ac893975c26e2eec613bcffbb4a1
Maxim Lyashenko <maximlyashenko@gmail.com>
2017-02-16 01:14:44 +0200

    Adding Db (code first)

---
+++ source/AkvelonTestApp.Data/Interfaces/IAppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using System.Data.Entity;
+
+using AkvelonTestApp.Data.Models;
+
+namespace AkvelonTestApp.Data.Interfaces
+{
+	public interface IAppDbContext
+	{
+		IDbSet<UserModel> Users { get; set; }
+		int SaveChanges();
+	}
+}
---
+++ source/AkvelonTestApp.Data/Models/UserModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AkvelonTestApp.Data.Models
+{
+	[Table("Users")]
+	public class UserModel
+	{
+		[Key]
+		[Required]
+		[Display(Description = "Nick Name")]
+		[RegularExpression(@"^([a-zA-Z0-9]+)$", ErrorMessage = "Invalid NickName")]
+		public virtual string NickName { get; set; }
+
+		[Required]
+		[Display(Description = "Full Name")]
+		public virtual string FullName { get; set; }
+	}
+}
